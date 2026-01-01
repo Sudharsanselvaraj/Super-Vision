7dc6804173f42c01807c936f2c9619db9b00e03d
Sudharsan <ss0856@srmist.edu.in>
2026-01-01 20:24:00 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI, WebSocket
-import cv2
-import numpy as np
-import base64
-
-from vision_engine import analyze_frame
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.websocket("/ws/student")
 async def student_ws(ws: WebSocket):
     await ws.accept()
     while True:
-        data = await ws.receive_text()
-        img_bytes = base64.b64decode(data)
-        np_img = np.frombuffer(img_bytes, np.uint8)
-        frame = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
-
-        result = analyze_frame(frame)
-        await ws.send_json(result)
+        data = await ws.receive_json()
+        await ws.send_json(data)
